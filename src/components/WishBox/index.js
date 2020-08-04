feb601dee6b4ca156392ea7a48d55e3b8e990f69
Habib Kadiri <kadirit@hotmail.com>
2020-08-04 18:00:22 +0200

    Initial Setup

--- src/components/WishBox/index.js
+++ src/components/WishBox/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import upload from '../../assets/upload.png'
 
 const Container = styled.div`
   background-color: #00d6ab;
@@ -11,6 +12,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
   
   h1{
     font-size: 25px;
@@ -18,21 +20,50 @@ const Container = styled.div`
 `
 const BaseInputArea = styled.div`
    width: 100%;
-   height: 150px;
+   //height: 110px;
    background-color: white;
    border-radius: 5px;
    border: 2px solid #868686;
+   padding: 10px;
 `
 
-const AreaOne = styled(BaseInputArea)`
+const Upload = styled.img``
+
+const FileInput = styled.input`
+  display: none;
+`
+
+const TopArea = styled(BaseInputArea)`
   margin-top: 10px;
-  h1{
   
+  label {
+    color: #999999;
+    font-size: 20px;
   }
+`
+
+const MiddleArea = styled(BaseInputArea)`
+   height: 200px;
+`
+
+const BottomArea = styled.div`
+   height: 100px;
+      width: 100%;
+      padding: 10px;
+      button{
+        
+      }
+
 `
 const BaseInput = styled.input`
    padding: 10px;
    width: 100%;
+   outline: transparent;
+   border: none;
+`
+const CompInput = styled(BaseInput)`
+  padding: 0;
+  margin-top: 10px;
 `
 
 
@@ -43,9 +74,17 @@ const WishBox = (props) => {
     return(
         <Container>
             <h1>Wish for a Company</h1>
-            <AreaOne>
-                <h1>Company Name</h1>
-            </AreaOne>
+            <TopArea>
+                <label htmlFor="name">Company Name:</label>
+                <CompInput id="name"/>
+            </TopArea>
+            <MiddleArea>
+
+            </MiddleArea>
+            <BottomArea>
+                <Upload src={upload}/>
+                <FileInput/>
+            </BottomArea>
         </Container>
     )
 }
